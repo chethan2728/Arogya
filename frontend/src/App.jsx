4f31427ec32dc19553660e2992aa7deef9fa4bf0
chethan <nchethan250@gmail.com>
2025-10-04 22:08:56 +0530

    Fix: corrected NavBar import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 import MyProfile from "./pages/MyProfile";
 import MyAppointments from "./pages/MyAppointments";
 import Appointment from "./pages/Appointment";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 
