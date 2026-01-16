45006d7d5575502eed0e6f9fc0f91f345eb3ca07
chethan kumar p s <chethankumarps@chethans-MacBook-Air.local>
2026-01-17 00:31:12 +0530

    second commit of Arogya project

--- frontend/src/pages/Docters.jsx
+++ frontend/src/pages/Docters.jsx
@@ -29,8 +29,8 @@ useEffect(()=>{
       <p className='text-gray-600'>Browse through the doctors specialist</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
         <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={()=>setShowFilter(prev => !prev)}>Filters</button>
-      <div className='flex flex-col gap-4 text-sm text-gray-600'>
-        <p onClick={()=> speciality === 'General physician' ? navigate('/docters') : navigate('/docters/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
+      <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
+        <p onClick={()=> speciality === 'General physician' ? navigate('/doctors') : navigate('/docters/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
         <p onClick={()=> speciality === 'Gynecologist' ? navigate('/docters') : navigate('/docters/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
         <p onClick={()=> speciality === 'Dermatologist' ? navigate('/docters') : navigate('/docters/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
         <p onClick={()=> speciality === 'Pediatricians' ? navigate('/docters') : navigate('/docters/Pediatricians')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Pediatricians" ? "bg-indigo-100 text-black" : ""}`}>Pediatricians</p>
